938184cffc7d886acf8421634341e31898bb37bf
Octopus Octopus <oct2pus@jade.moe>
2021-10-09 19:31:21 -0500

    remove jademoe variant

--- app/javascript/flavours/jademoe/packs/home.js
+++
@@ -1,8 +0,0 @@
-import 'packs/public-path';
-import loadPolyfills from 'flavours/glitch/util/load_polyfills';
-
-loadPolyfills().then(() => {
-  require('flavours/glitch/util/main').default();
-}).catch(e => {
-  console.error(e);
-});
